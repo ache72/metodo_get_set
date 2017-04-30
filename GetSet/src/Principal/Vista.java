a2cc58cf3d1d0f4fdbf0fe1e65ef524573c7e5ac
obeddesign <adrian.chuquiray@gmail.com>
2017-04-30 10:18:11 -0500

    trabajando con los metodos get y set

--- GetSet/src/Principal/Vista.java
+++ GetSet/src/Principal/Vista.java
@@ -4,7 +4,8 @@ package Principal;
 public class Vista {
 
     public static void main(String[] args) {
-        
+        Metodos.GetSet obj = new Metodos.GetSet(123456, "adrian", 30, "el principe");
+        System.out.println(obj.npaginas);
     }
     
 }
